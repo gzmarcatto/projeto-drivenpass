7ca7e8e88fa78ec04ddfc19ace75f49b9cd5d7cb
Giovanni Marcatto <gzmarcatto@gmail.com>
2023-05-09 16:54:35 -0300

    fix: user schema and service

--- src/services/users-service/index.ts
+++ src/services/users-service/index.ts
@@ -1,14 +1,10 @@
 import { User } from '@prisma/client';
 import bcrypt from 'bcrypt';
-import eventsService from '../events-service';
 import { duplicatedEmailError } from './errors';
 import userRepository from '@/repositories/user-repository';
-import { cannotEnrollBeforeStartDateError } from '@/errors';
 
 export async function createUser({ email, password }: CreateUserParams): Promise<User> {
-  await canEnrollOrFail();
-
-  await validateUniqueEmailOrFail(email);
+  await validateUniqueEmail(email);
 
   const hashedPassword = await bcrypt.hash(password, 12);
   return userRepository.create({
@@ -17,19 +13,13 @@ export async function createUser({ email, password }: CreateUserParams): Promise
   });
 }
 
-async function validateUniqueEmailOrFail(email: string) {
+async function validateUniqueEmail(email: string) {
   const userWithSameEmail = await userRepository.findByEmail(email);
   if (userWithSameEmail) {
     throw duplicatedEmailError();
   }
 }
 
-async function canEnrollOrFail() {
-  const canEnroll = await eventsService.isCurrentEventActive();
-  if (!canEnroll) {
-    throw cannotEnrollBeforeStartDateError();
-  }
-}
 
 export type CreateUserParams = Pick<User, 'email' | 'password'>;
 
